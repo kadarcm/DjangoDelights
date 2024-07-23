a1acc6d4e6b1e31598e92746501618395a9ea69e
ckadar <kadarcm@gmail.com>
2024-07-22 22:12:48 -0400

    debug setting

--- Dj_delights/settings.py
+++ Dj_delights/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0-v+^qhe%(dakiqhg066+p$r=5pjl(zdbu!8n!t^fq2pi)wl#2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
